c397c106bcfdcd321c9b0e8788a9ef322064b869
adeick8 <107136708+adeick8@users.noreply.github.com>
2022-06-24 12:20:10 -0500

    Missed a Plugin

---
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Intermediate/Build/Android/UE4/Inc/VRExpansionPlugin/VRWheeledVehicle.generated.h
@@ -0,0 +1,108 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class AController;
+#ifdef VREXPANSIONPLUGIN_VRWheeledVehicle_generated_h
+#error "VRWheeledVehicle.generated.h already included, missing '#pragma once' in VRWheeledVehicle.h"
+#endif
+#define VREXPANSIONPLUGIN_VRWheeledVehicle_generated_h
+
+#define summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_SPARSE_DATA
+#define summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execForceSecondaryPossession); \
+	DECLARE_FUNCTION(execSetOverrideController); \
+	DECLARE_FUNCTION(execSetBindToInput);
+
+
+#define summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execForceSecondaryPossession); \
+	DECLARE_FUNCTION(execSetOverrideController); \
+	DECLARE_FUNCTION(execSetBindToInput);
+
+
+#define summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAVRWheeledVehicle(); \
+	friend struct Z_Construct_UClass_AVRWheeledVehicle_Statics; \
+public: \
+	DECLARE_CLASS(AVRWheeledVehicle, AWheeledVehicle, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(AVRWheeledVehicle)
+
+
+#define summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_INCLASS \
+private: \
+	static void StaticRegisterNativesAVRWheeledVehicle(); \
+	friend struct Z_Construct_UClass_AVRWheeledVehicle_Statics; \
+public: \
+	DECLARE_CLASS(AVRWheeledVehicle, AWheeledVehicle, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(AVRWheeledVehicle)
+
+
+#define summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVRWheeledVehicle(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRWheeledVehicle) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRWheeledVehicle); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRWheeledVehicle); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRWheeledVehicle(AVRWheeledVehicle&&); \
+	NO_API AVRWheeledVehicle(const AVRWheeledVehicle&); \
+public:
+
+
+#define summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVRWheeledVehicle(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRWheeledVehicle(AVRWheeledVehicle&&); \
+	NO_API AVRWheeledVehicle(const AVRWheeledVehicle&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRWheeledVehicle); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRWheeledVehicle); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRWheeledVehicle)
+
+
+#define summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_PRIVATE_PROPERTY_OFFSET
+#define summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_27_PROLOG
+#define summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_PRIVATE_PROPERTY_OFFSET \
+	summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_SPARSE_DATA \
+	summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_RPC_WRAPPERS \
+	summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_INCLASS \
+	summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_PRIVATE_PROPERTY_OFFSET \
+	summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_SPARSE_DATA \
+	summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_RPC_WRAPPERS_NO_PURE_DECLS \
+	summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_INCLASS_NO_PURE_DECLS \
+	summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h_33_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VREXPANSIONPLUGIN_API UClass* StaticClass<class AVRWheeledVehicle>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID summer_2022_vr_project_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRWheeledVehicle_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
